396de45a88754496f420e0754b96ad0e05d479e7
9551Dev <oliverekcaha@gmail.com>
2022-10-17 21:57:33 +0200

    gonna test the installer

---
+++ installer.lua
@@ -0,0 +1,40 @@
+fs.makeDir("C3D")
+local github_api = http.get(
+	"https://api.github.com/repos/9551-Dev/C3D/git/trees/main?recursive=1",
+	_G._GIT_API_KEY and {Authorization = 'token ' .. _G._GIT_API_KEY}
+)
+
+local list = textutils.unserialiseJSON(github_api.readAll())
+local ls = {}
+local len = 0
+github_api.close()
+for k,v in pairs(list.tree) do
+    if v.type == "blob" and v.path:lower():match(".+%.lua") then
+        ls["https://raw.githubusercontent.com/9551-Dev/C3D/main/"..v.path] = v.path
+        len = len + 1
+    end
+end
+local percent = 100/len
+local finished = 0
+local size_gained = 0
+local downloads = {}
+for k,v in pairs(ls) do
+    table.insert(downloads,function()
+        local web = http.get(k)
+        local file
+            file = fs.open("./c3d.lua","w")
+        if v == "c3d.lua" then
+        else
+            file = fs.open("./C3D/"..v,"w")
+        end
+        local wd = web.readAll()
+        file.write(wd)
+        file.close()
+        web.close()
+        finished = finished + 1
+        size_gained = size_gained + #("%q"):format(wd)
+        print("downloading "..v.."  "..tostring(math.ceil(finished*percent)).."% "..tostring(math.ceil(file_size/1024*10)/10).."kB total: "..math.ceil(size_gained/1024).."kB")
+    end)
+end
+parallel.waitForAll(table.unpack(downloads))
+print("Finished downloading GuiH")
