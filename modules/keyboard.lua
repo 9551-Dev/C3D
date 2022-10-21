c8fd055eaa263e63abd4eb8e274c961821b4d72e
Oliver Caha <74534592+9551-Dev@users.noreply.github.com>
2022-10-21 17:22:17 +0200

    Update keyboard.lua

--- modules/keyboard.lua
+++ modules/keyboard.lua
@@ -1,12 +1,6 @@
 local keyboard = {}
 
 return function(BUS)
-    function keyboard.get_key_repeat()
-        return BUS.keyboard.key_reapeat
-    end
-    function keyboard.set_key_repeat(enable)
-        BUS.keyboard.key_reapeat = enable
-    end
 
     function keyboard.has_text_input()
         return BUS.keyboard.textinput
@@ -25,8 +19,6 @@ return function(BUS)
         end
         return true
     end
-
-    keyboard.isScancodeDown = keyboard.isDown
 
     return keyboard
 end
