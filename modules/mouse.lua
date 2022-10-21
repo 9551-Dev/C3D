9c44735d52a45f698b3a342c9e26e9941f5d4956
Oliver Caha <74534592+9551-Dev@users.noreply.github.com>
2022-10-21 21:26:18 +0200

    changed up to the standard :sunglasses:

--- modules/mouse.lua
+++ modules/mouse.lua
@@ -1,12 +1,12 @@
 local mouse = {}
 
 return function(BUS)
-    function mouse.getPosition()
+    function mouse.get_position()
         return BUS.mouse.last_x, BUS.mouse.last_y
     end
-    function mouse.getX() return BUS.mouse.last_x end
-    function mouse.getY() return BUS.mouse.last_y end
-    function mouse.isDown(...)
+    function mouse.get_x() return BUS.mouse.last_x end
+    function mouse.get_y() return BUS.mouse.last_y end
+    function mouse.is_down(...)
         local btn_list = {...}
         for k,key in pairs(btn_list) do
             local held = BUS.mouse.held[key]
@@ -14,11 +14,11 @@ return function(BUS)
         end
         return true
     end
-    function mouse.setPosition(x,y)
+    function mouse.set_position(x,y)
         BUS.mouse.last_x = x
         BUS.mouse.last_y = y
     end
-    function mouse.setX(x) BUS.mouse.last_x = x end
-    function mouse.setY(y) BUS.mouse.last_y = y end
+    function mouse.set_x(x) BUS.mouse.last_x = x end
+    function mouse.set_y(y) BUS.mouse.last_y = y end
     return mouse
 end
