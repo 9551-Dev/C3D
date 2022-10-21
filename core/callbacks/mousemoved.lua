277882c0f35e43591facf680c69713ad52b89386
Oliver Caha <74534592+9551-Dev@users.noreply.github.com>
2022-10-21 17:27:12 +0200

    Update mousemoved.lua

--- core/callbacks/mousemoved.lua
+++ core/callbacks/mousemoved.lua
@@ -2,10 +2,10 @@ return {ev="mouse_move",run=function(BUS,caller,ev,_,x,y)
     if x ~= nil then
         local change_x = x - (BUS.mouse.last_x or 0)
         local change_y = y - (BUS.mouse.last_y or 0)
-        BUS.events[#BUS.events+1] = {"mousemoved",x*2,y*3,change_x,change_y,false}
+        BUS.events[#BUS.events+1] = {"mousemoved",x,y,change_x,change_y,false}
         BUS.mouse.last_x,BUS.mouse.last_y = x,y
         if type(caller.mousemoved) == "function" then
-            caller.mousemoved(x*2,y*3,change_x,change_y,false)
+            caller.mousemoved(x,y,change_x,change_y,false)
         end
     end
 end,check_change=function(self,BUS,caller,x,y)
