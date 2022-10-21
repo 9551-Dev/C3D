077fb340c4f2b0ae299959ef63d72b41805e5451
Oliver Caha <74534592+9551-Dev@users.noreply.github.com>
2022-10-21 15:58:00 +0200

    Update keyreleased.lua

--- core/callbacks/keyreleased.lua
+++ core/callbacks/keyreleased.lua
@@ -2,6 +2,6 @@ return {ev="key_up",run=function(BUS,caller,ev,key_code)
     local code = keys.getName(key_code)
     BUS.events[#BUS.events+1] = {"keyreleased",code,code}
     if type(caller.keyreleased) == "function" then
-        caller.keyreleased(code,code)
+        caller.keyreleased(code)
     end
 end}
