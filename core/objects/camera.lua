cda44983ae2c6765c7eeb3746054a0c195988a92
9551Dev <oliverekcaha@gmail.com>
2022-10-30 14:50:57 +0100

    somewhat fixed things ig

--- core/objects/camera.lua
+++ core/objects/camera.lua
@@ -1,18 +1,16 @@
 local object = require("core.object")
 
-local translate_matrix        = require("core.3D.matrice.translate")
+local camera_translate_matrix = require("core.3D.matrice.camera_translate")
 local euler_rotation_matrix   = require("core.3D.matrice.rotation_euler")
 local quat_rotation_matrix    = require("core.3D.matrice.rotation_quaternion")
 local lookat_transform_matrix = require("core.3D.matrice.lookat")
-
-local matmul = require("core.3D.math.matmul")
 
 return {add=function(BUS)
 
     local camera_object = {
         __index = object.new{
             set_position = function(this,x,y,z)
-                this.position = translate_matrix(-x,-y,-z)
+                this.position = camera_translate_matrix(-x,-y,-z)
                 this.transform = nil
                 return this
             end,
@@ -39,7 +37,7 @@ return {add=function(BUS)
     return {new=function()
         local obj = {
             rotation = euler_rotation_matrix(0,0,0),
-            position = translate_matrix(0,0,0)
+            position = camera_translate_matrix(0,0,0)
         }
 
         return setmetatable(obj,camera_object):__build()
