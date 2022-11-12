8a1a2d943822277f2566da3e6883dc9c838c22a8
9551Dev <oliverekcaha@gmail.com>
2022-11-13 00:13:39 +0100

    caching seems to be a bad idea here

--- core/3D/geometry/interpolate_vertex.lua
+++ core/3D/geometry/interpolate_vertex.lua
@@ -9,18 +9,18 @@ return {init=function(BUS)
         local v15 = v1[5]
         local v17 = v1[7]
 
-        local vert = memory_handle.get_table()
+        local vert = {
+            (1 - alpha) * v1[1] + alpha * v2[1],
+            (1 - alpha) * v1[2] + alpha * v2[2],
+            (1 - alpha) * v1[3] + alpha * v2[3],
+            (1 - alpha) * v1[4] + alpha * v2[4],
+            0,0,0,0
+        }
 
-        vert[1] = (1 - alpha) * v1[1] + alpha * v2[1]
-        vert[2] = (1 - alpha) * v1[2] + alpha * v2[2]
-        vert[3] = (1 - alpha) * v1[3] + alpha * v2[3]
-        vert[4] = (1 - alpha) * v1[4] + alpha * v2[4]
-        vert[5],vert[6],vert[7],vert[8] = 0,0,0,0
-
-        if type(v15) == "number" then
+        if v15 then
             vert[5] = (1 - alpha) * v15   + alpha * (v2[5] or 0)
             vert[6] = (1 - alpha) * v1[6] + alpha * (v2[6] or 0)
-        end if type(v17) == "number" then
+        end if v17 then
             vert[7] = (1 - alpha) * v17   + alpha * (v2[7] or 0)
             vert[8] = (1 - alpha) * v1[8] + alpha * (v2[8] or 0)
         end
