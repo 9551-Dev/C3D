12bcc9e31f4c535c1d6a9a5410891d193f41e59e
Apethesis <apethesis@gmail.com>
2022-11-12 17:06:48 +0100

    Yet another "improvement"
    9551 i swear to god if you say this "wastes" memory i will throw you out the window

--- core/loaders/texture/cimg2.lua
+++ core/loaders/texture/cimg2.lua
@@ -3,16 +3,22 @@ local tbl = require("common.table_util")
 return {read=function(BUS,path_tex)
     local fl = fs.open(path_tex,"r")
     local map = tbl.createNDarray(1)
+    local retmap = tbl.createNDarray(1)
     local cm = textutils.unserialize(fl.readAll())
     for k1,v1 in pairs(cm) do
         for k2,v2 in pairs(v1) do
-            map[k2][k1] = 2^tonumber(string.sub(v2,2,2),16)
+            local tbl = {}
+            tbl[1] = string.sub(v2,1,1) -- c
+            tbl[2] = string.sub(v2,2,2) -- b
+            tbl[3] = string.sub(v2,3,3) -- f
+            map[k1][k2] = tbl
+            retmap[k2][k1] = 2^tonumber(tbl[2],16)
         end
     end
     local res = {
-        w=#map+1,
-        h=#map[#map]-1,
-        pixels=map
+        w=#map,
+        h=#map[#map],
+        pixels=retmap
     }
     return res
 end}
