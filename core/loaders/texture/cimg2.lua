6f9ef097b9500ea82313fff331bac6ce36c95ee3
Apethesis <apethesis@gmail.com>
2022-11-12 17:11:42 +0100

    yet another tiny improvement
    i hate you 9551

--- core/loaders/texture/cimg2.lua
+++ core/loaders/texture/cimg2.lua
@@ -8,9 +8,7 @@ return {read=function(BUS,path_tex)
     for k1,v1 in pairs(cm) do
         for k2,v2 in pairs(v1) do
             local tbl = {}
-            tbl[1] = string.sub(v2,1,1) -- c
             tbl[2] = string.sub(v2,2,2) -- b
-            tbl[3] = string.sub(v2,3,3) -- f
             map[k1][k2] = tbl
             retmap[k2][k1] = 2^tonumber(tbl[2],16)
         end
