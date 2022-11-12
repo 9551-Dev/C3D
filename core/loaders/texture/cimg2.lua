690f731444b43e3aa56b5aaa8ec072a4483f3875
Apethesis <apethesis@gmail.com>
2022-11-12 17:21:46 +0100

    Update cimg2.lua
    kill me

--- core/loaders/texture/cimg2.lua
+++ core/loaders/texture/cimg2.lua
@@ -7,10 +7,9 @@ return {read=function(BUS,path_tex)
     local cm = textutils.unserialize(fl.readAll())
     for k1,v1 in pairs(cm) do
         for k2,v2 in pairs(v1) do
-            local tbl = {}
-            tbl[2] = string.sub(v2,2,2) -- b
-            map[k1][k2] = tbl
-            retmap[k2][k1] = 2^tonumber(tbl[2],16)
+            local val = string.sub(v2,2,2) -- b
+            map[k1][k2] = val
+            retmap[k2][k1] = 2^tonumber(val,16)
         end
     end
     local res = {
